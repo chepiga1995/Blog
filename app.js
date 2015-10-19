608b28c066f7a4e2aef99ca5c1f6421a47de72da
chepiga1995 <ura1995ura@gmail.com>
2015-10-19 06:21:27 +0300

    add directories

--- app.js
+++ app.js
@@ -2,13 +2,14 @@ var express = require('express');
 var app = express();
 var config = require('./config');
 
-
 app.set('env', config.get("NODE_ENV"));
 app.use(require('morgan')('dev', {
     skip: function(){
         return app.get('env') != 'development';
     }
 }));
+app.use(express.static('public'));
+
 app.get('/', function(req, res, next){
     res.end('Hello World!!');
 });
