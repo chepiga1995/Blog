e146130e71637682d44c381051661f3e3b34baa0
chepiga1995 <ura1995ura@gmail.com>
2015-10-19 05:41:09 +0300

    add configurating node_env through comand line environment

--- config/index.js
+++ config/index.js
@@ -2,9 +2,14 @@ var fs    = require('fs');
 var nconf = require('nconf');
 
 nconf.argv()
-    .env()
-    .file({ file: __dirname + '/package.json' });
+    .env();
 
+var env = nconf.get('NODE_ENV');
 
+nconf.file({ file: __dirname + '/package.json' });
+
+if (env){
+    nconf.set('NODE_ENV', env);
+}
 
 module.exports = nconf;
